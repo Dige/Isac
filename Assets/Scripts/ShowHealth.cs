2328f054b42daf0aa909b9d2a70c563f19141c02
Vesa Paakkanen <vesapaakkanen@gmail.com>
2014-10-06 19:08:56 +0300

    Implemented displaying of health

---
+++ Assets/Scripts/ShowHealth.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class ShowHealth : MonoBehaviour {
+
+    [SerializeField]
+    private Texture2D _healthTexture;
+    public Texture2D HealthTexture
+    {
+        get { return _healthTexture; }
+        set { _healthTexture = value; }
+    }
+
+    [SerializeField]
+    private Texture2D _halfHealthTexture;
+    public Texture2D HalfHealthTexture
+    {
+        get { return _halfHealthTexture; }
+        set { _halfHealthTexture = value; }
+    }
+
+    private CharacterController _playerCharacter;
+
+    public void Start()
+    {
+        _playerCharacter = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterController>();
+    }
+
+    public void OnGUI()
+    {
+        for (int i = 0; i < _playerCharacter.Health / 2; i++)
+        {
+            GUI.DrawTexture(new Rect(Screen.width - 250 + i*40,30,40,40), _healthTexture, ScaleMode.ScaleToFit);    
+        }
+
+        if (_playerCharacter.Health % 2 == 1)
+        {
+            GUI.DrawTexture(new Rect(Screen.width - 250 + _playerCharacter.Health / 2 * 40, 30, 40, 40), _halfHealthTexture, ScaleMode.ScaleToFit);
+        }
+    }
+}
