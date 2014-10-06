d8944441e558211911dc7ec23235fab13c717a9a
Vesa Paakkanen <vesapaakkanen@gmail.com>
2014-10-06 19:56:01 +0300

    Implemented a simple enemy

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,153 +1,70 @@
-﻿using UnityEngine;
+﻿using Assets.Scripts;
+using UnityEngine;
 
-public class CharacterController : MonoBehaviour
-{
-    [SerializeField]
-    private float _moveSpeed;
+public class CharacterController : CharacterControllerBase
+{   
     private Vector2 _momentum;
-    public float MoveSpeed
-    {
-        get { return _moveSpeed; }
-        set { _moveSpeed = value; }
-    }
 
-    [SerializeField]
-    private float _bulletSpeed = 0.5f;
-    public float BulletSpeed
-    {
-        get { return _bulletSpeed; }
-        set { _bulletSpeed = value;  }
-    }
-
-    private Vector3 _moveDirection;
-    private Vector3 _target;
-    private bool _shouldMove;
-
-    private Animator _animator;
-
-    [SerializeField]
-    private Rigidbody2D _bulletPrefab;
-    public Rigidbody2D BulletPrefab
-    {
-        get { return _bulletPrefab; }
-        set { _bulletPrefab = value; }
-    }
-
-    [SerializeField]
-    private int _health = 8;
-    public int Health
-    {
-        get { return _health; }
-        set { _health = value; }
-    }
-
-
-    public void Start()
-    {
-        _moveDirection = Vector3.down;
-        _animator = GetComponent<Animator>();
-        _animator.enabled = false;
-    }
-
-    public void Update()
-    {
-        HandleMovement();
-        HandleShooting();
-    }
-
-    private void HandleShooting()
+    protected override void HandleShooting()
     {
         if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow)
             || Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
         {
-            var bullet = (Rigidbody2D)Instantiate(_bulletPrefab);
+            var bullet = (Rigidbody2D)Instantiate(BulletPrefab);
             bullet.transform.position = transform.position;
             if (Input.GetKeyUp(KeyCode.UpArrow))
             {
                 bullet.transform.Rotate(0, 0, -90);
-                bullet.AddForce(new Vector2(0, _bulletSpeed));
+                bullet.AddForce(new Vector2(0, BulletSpeed));
             }
             else if (Input.GetKeyUp(KeyCode.DownArrow))
             {
                 bullet.transform.Rotate(0, 0, 90);
-                bullet.AddForce(new Vector2(0, -_bulletSpeed));
+                bullet.AddForce(new Vector2(0, -BulletSpeed));
             }
             else if (Input.GetKeyUp(KeyCode.LeftArrow))
             {
-                bullet.AddForce(new Vector2(-_bulletSpeed, 0));
+                bullet.AddForce(new Vector2(-BulletSpeed, 0));
             }
             else if (Input.GetKeyUp(KeyCode.RightArrow))
             {
                 bullet.transform.Rotate(0, 0, -180);
-                bullet.AddForce(new Vector2(_bulletSpeed, 0));
+                bullet.AddForce(new Vector2(BulletSpeed, 0));
             }
             bullet.AddForce(_momentum*0.001f);
         }
     }
 
-    private const float ANIMATION_STOP_VELOCITY = 0.35f;
-
-    private void HandleMovement()
+    protected override Vector3 DetermineMovement()
     {
-        Vector3 currentPosition = transform.position;
-
-        _shouldMove = false;
-        _target = currentPosition;
-
         var movement = new Vector3();
 
         if (Input.GetKey("w"))
         {
             movement += new Vector3(0, 1, 0);
-            _shouldMove = true;
+            ShouldMove = true;
         }
         if (Input.GetKey("s"))
         {
             movement += new Vector3(0, -1, 0);
-            _shouldMove = true;
+            ShouldMove = true;
         }
         if (Input.GetKey("a"))
         {
             movement += new Vector3(-1, 0, 0);
-            _shouldMove = true;
+            ShouldMove = true;
         }
         if (Input.GetKey("d"))
         {
             movement += new Vector3(1, 0, 0);
-            _shouldMove = true;
+            ShouldMove = true;
         }
+        return movement;
+    }
 
-        if (_animator.enabled && gameObject.rigidbody2D.velocity.x < ANIMATION_STOP_VELOCITY && gameObject.rigidbody2D.velocity.x > -ANIMATION_STOP_VELOCITY
-            && gameObject.rigidbody2D.velocity.y < ANIMATION_STOP_VELOCITY && gameObject.rigidbody2D.velocity.y > -ANIMATION_STOP_VELOCITY)
-        {
-            _animator.enabled = false;
-            return;
-        }
-
-        if (!_shouldMove)
-            return;
-
-        _target += movement;
-        _moveDirection = _target - currentPosition;
-        _moveDirection.z = _target.z = 0;
-        _moveDirection.Normalize();
-        _animator.enabled = true;
-
-        //Vector3 target = _moveDirection * MoveSpeed + currentPosition;
-        //transform.position = currentPosition = Vector3.Lerp(currentPosition, target, Time.deltaTime);
+    protected override void HandleMovement(Vector3 movement)
+    {
         _momentum = movement * MoveSpeed;
         gameObject.rigidbody2D.AddForce(_momentum);
-        if (movement.x != 0.0f && movement.y != 0.0f)
-        {
-            _animator.SetInteger("Direction", _target.y > currentPosition.y ? 0 : 2);
-        }
-        else if (movement.x != 0.0f && movement.y == 0.0f)
-        {
-            _animator.SetInteger("Direction", _target.x > currentPosition.x ? 1 : 3);
-        }
-        else if (movement.x == 0.0f && movement.y != 0.0f)
-        {
-            _animator.SetInteger("Direction", _target.y > currentPosition.y ? 0 : 2);
-        }
     }
 }
---
+++ Assets/Scripts/CharacterControllerBase.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public abstract class CharacterControllerBase : MonoBehaviour
+    {
+        [SerializeField]
+        private float _moveSpeed;
+        public float MoveSpeed
+        {
+            get { return _moveSpeed; }
+            set { _moveSpeed = value; }
+        }
+
+        [SerializeField]
+        private float _bulletSpeed = 0.5f;
+        public float BulletSpeed
+        {
+            get { return _bulletSpeed; }
+            set { _bulletSpeed = value; }
+        }
+
+        protected bool ShouldMove;
+
+        protected Animator Animator;
+
+        [SerializeField]
+        private Rigidbody2D _bulletPrefab;
+        public Rigidbody2D BulletPrefab
+        {
+            get { return _bulletPrefab; }
+            set { _bulletPrefab = value; }
+        }
+
+        [SerializeField]
+        private int _health = 8;
+        public int Health
+        {
+            get { return _health; }
+            set { _health = value; }
+        }
+
+
+        public virtual void Start()
+        {
+            Animator = GetComponent<Animator>();
+            Animator.enabled = false;
+        }
+
+        public virtual void Update()
+        {
+            var movement = DetermineMovement();
+            HandleAnimation(movement);
+            HandleMovement(movement);
+            HandleShooting();
+        }
+
+        protected abstract void HandleMovement(Vector3 movement);
+
+        protected abstract void HandleShooting();
+        protected abstract Vector3 DetermineMovement();
+
+        private const float ANIMATION_STOP_VELOCITY = 0.35f;
+
+        protected virtual void HandleAnimation(Vector3 movement)
+        {
+            Vector3 currentPosition = transform.position;
+
+            if (Animator.enabled && gameObject.rigidbody2D.velocity.x < ANIMATION_STOP_VELOCITY && gameObject.rigidbody2D.velocity.x > -ANIMATION_STOP_VELOCITY
+                && gameObject.rigidbody2D.velocity.y < ANIMATION_STOP_VELOCITY && gameObject.rigidbody2D.velocity.y > -ANIMATION_STOP_VELOCITY)
+            {
+                Animator.enabled = false;
+                return;
+            }
+
+            if (!ShouldMove)
+                return;
+
+            var target = currentPosition + movement;
+            Animator.enabled = true;
+
+            if (movement.x != 0.0f && movement.y != 0.0f)
+            {
+                Animator.SetInteger("Direction", target.y > currentPosition.y ? 0 : 2);
+            }
+            else if (movement.x != 0.0f && movement.y == 0.0f)
+            {
+                Animator.SetInteger("Direction", target.x > currentPosition.x ? 1 : 3);
+            }
+            else if (movement.x == 0.0f && movement.y != 0.0f)
+            {
+                Animator.SetInteger("Direction", target.y > currentPosition.y ? 0 : 2);
+            }
+        }
+    }
+}
