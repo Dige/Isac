d16fe689828527e1b5914d99a95f10bd743ab6c1
jperamak <jussi.peramaki@gmail.com>
2014-10-21 18:49:43 +0300

    enemy layouts

---
+++ Assets/Scripts/EnemyLayout.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Assets.Scripts;
+using UnityEngine;
+using Random = UnityEngine.Random;
+public class EnemyLayout : MonoBehaviour {
+
+
+	[SerializeField]
+	private List<GameObject> _enemyLayouts = new List<GameObject>();
+	public List<GameObject> EnemyLayouts
+	{
+		get { return _enemyLayouts; }
+	}
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/EnemyShootController.cs
+++ Assets/Scripts/EnemyShootController.cs
@@ -24,6 +24,7 @@ namespace Assets.Scripts
 			base.Start ();
 			_player = GameObject.FindGameObjectWithTag ("Player");
 			_shooting = false;
+
 		}
 
 		public override void Update()
