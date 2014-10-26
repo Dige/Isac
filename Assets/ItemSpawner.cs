a970255dda92f18d192399524281a444a5fa230e
Vesa Paakkanen <vesapaakkanen@gmail.com>
2014-10-26 12:27:33 +0200

    Items for treasure room

---
+++ Assets/ItemSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Assets.Scripts;
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour 
+{
+
+    [SerializeField]
+    private List<ItemBase> _itemPrefabs = new List<ItemBase>();
+    public List<ItemBase> ItemPrefabs
+    {
+        get { return _itemPrefabs; }
+    }
+
+	public void Awake ()
+	{
+	    var item = (ItemBase)Instantiate(_itemPrefabs[Random.Range(0, _itemPrefabs.Count - 1)]);
+	    item.transform.parent = transform;
+	    item.transform.localPosition = Vector3.zero;
+	}
+	
+}
