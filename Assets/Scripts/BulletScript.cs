4cef0bee337cf8a646832504abeb653992442e29
Vesa Paakkanen <vesapaakkanen@gmail.com>
2014-10-26 15:46:32 +0200

    Hack to fix vertical shooting when near wall

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -37,6 +37,8 @@ public class BulletScript : MonoBehaviour {
 
     public void Start()
     {
+        GetComponent<BoxCollider2D>().enabled = false;
+        StartCoroutine(RemoveInvulnerability());
 		if (Shooter.CompareTag("Enemy")) {
 			transform.gameObject.layer = LayerMask.NameToLayer( "Enemy bullet" );
 		}
@@ -44,6 +46,12 @@ public class BulletScript : MonoBehaviour {
         var p = GameObject.FindWithTag("Player");
         var pc = p.GetComponent<Player>();
         range = pc.Range;
+    }
+
+    private IEnumerator RemoveInvulnerability()
+    {
+        yield return new WaitForSeconds(0.05f);
+        GetComponent<BoxCollider2D>().enabled = true;
     }
 
     private bool _isFading;
