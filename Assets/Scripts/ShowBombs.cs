e8576dd7f6aaeb56560cc9cbc7177108332c4b10
Vesa Paakkanen <vesapaakkanen@gmail.com>
2014-10-28 22:14:30 +0200

    Show bombs and updated ItemSpawner

---
+++ Assets/Scripts/ShowBombs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class ShowBombs : MonoBehaviour
+    {
+        private PlayerShootController _player;
+
+        public void Start()
+        {
+            _player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerShootController>();
+        }
+
+        public void OnGUI()
+        {
+            if (_player == null || _player.BombPrefab == null)
+            {
+                return;
+            }
+
+            var iconTexture = _player.BombPrefab.GetComponent<SpriteRenderer>().sprite.texture;
+            var iconWidth = Screen.width / 17;
+            var iconHeight = Screen.height / 17;
+
+            GUI.DrawTexture(new Rect(Screen.width - Screen.width / 1.5f, Screen.height / 11.0f, iconWidth, iconHeight), iconTexture, ScaleMode.ScaleToFit);
+            GUI.Label(new Rect(Screen.width - Screen.width / 1.5f + iconWidth, Screen.height / 11.0f + iconHeight/4.0f, iconWidth, iconHeight), "x" + _player.NumberofBombs);
+        }
+    }
+}
